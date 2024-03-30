681ef79616b3027a30f0c2c47db96f43fdd04b41
Airton <airtonluizcanalefilho@hotmail.com>
2024-03-30 15:17:10 -0300

    refactor: change calculator buttons from position absolute to display flex

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
 function insert(num) {
-    var numero = (document.getElementById('resultado')).innerHTML;
-    (document.getElementById('resultado')).innerHTML = numero + num;
+    var numero = (document.getElementById('result')).innerHTML;
+    (document.getElementById('result')).innerHTML = numero + num;
 }
 
 function clean() {
-    (document.getElementById('resultado')).innerHTML = "";
+    (document.getElementById('result')).innerHTML = "";
 }
 function back() {
-    var resultado = (document.getElementById('resultado')).innerHTML;
-    (document.getElementById('resultado')).innerHTML = resultado.substring(0, resultado.length - 1);
+    var resultado = (document.getElementById('result')).innerHTML;
+    (document.getElementById('result')).innerHTML = resultado.substring(0, resultado.length - 1);
 }
 function calcular() {
-    var resultado = (document.getElementById('resultado')).innerHTML;
+    var resultado = (document.getElementById('result')).innerHTML;
     if (resultado) {
-        (document.getElementById('resultado')).innerHTML = eval(resultado);
+        (document.getElementById('result')).innerHTML = eval(resultado);
     }
     else {
-        (document.getElementById('resultado')).innerHTML = "";
+        (document.getElementById('result')).innerHTML = "";
     }
 }
