6b1511ff0b7e5bba40a701ae06b037a6fadca763
Airton <airtonluizcanalefilho@hotmail.com>
2024-03-31 13:22:57 -0300

    refactor: figma layout borders

--- calculator.js
+++ calculator.js
@@ -1,21 +1,26 @@
 function insert(num) {
-    var numero = (document.getElementById('result')).innerHTML;
-    (document.getElementById('result')).innerHTML = numero + num;
+  var numero = document.querySelector('.counts-header').innerHTML
+  document.querySelector('.counts-header').innerHTML = numero + num
 }
 
 function clean() {
-    (document.getElementById('result')).innerHTML = "";
+  document.getElementById('result').innerHTML = ''
+  document.querySelector('.counts-header').innerHTML = ''
 }
 function back() {
-    var resultado = (document.getElementById('result')).innerHTML;
-    (document.getElementById('result')).innerHTML = resultado.substring(0, resultado.length - 1);
+  var resultado = querySelector('.counts-header').innerHTML
+  querySelector('.counts-header').innerHTML = resultado.substring(
+    0,
+    resultado.length - 1
+  )
 }
 function calcular() {
-    var resultado = (document.getElementById('result')).innerHTML;
-    if (resultado) {
-        (document.getElementById('result')).innerHTML = eval(resultado);
-    }
-    else {
-        (document.getElementById('result')).innerHTML = "";
-    }
+  var resultado = document.querySelector('.counts-header').innerHTML
+  console.log(resultado)
+  if (resultado) {
+    document.getElementById('result').innerHTML = eval(resultado)
+  } else {
+    document.getElementById('result').innerHTML = ''
+    document.querySelector('.counts-header').innerHTML = ''
+  }
 }
