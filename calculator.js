5899a6b3c659dc822b26784d11d4f24c09b1377b
Airton <airtonluizcanalefilho@hotmail.com>
2024-04-08 15:02:28 -0300

    feat: add percentage button functionality

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@ const insert = (num) =>  {
   document.querySelector('.counts-header').innerHTML = numero + num
 }
 
-const clean = ()  => {
+const clear = ()  => {
   document.getElementById('result').innerHTML = ''
   document.querySelector('.counts-header').innerHTML = ''
 }
@@ -15,10 +15,37 @@ const back = ()  => {
   )
 }
 
-const calcular = ()  => {
-  const resultado = document.querySelector('.counts-header').innerHTML
-  if (resultado) {
-    document.getElementById('result').innerHTML = eval(resultado)
+const addPercentage = () => {
+  const numero = document.querySelector('.counts-header').innerHTML
+  document.querySelector('.counts-header').innerHTML = numero + '%'
+}
+
+const verifyPercentage = () => {
+  const header = document.querySelector('.counts-header').innerHTML
+  const countSplitted = header.split('%')
+  if(countSplitted[1]) {
+    const countResult =  (countSplitted[0] * countSplitted[1]) / 100
+    document.getElementById('result').innerHTML = eval(countResult)
+  }else {
+    document.getElementById('result').innerHTML = eval(header)
+  }
+
+}
+
+const addParentheses = () => {
+  let equation = document.querySelector('.counts-header').innerHTML
+  const equationSplitted = equation.split('')
+  if(equationSplitted.some(str => str === "(")) {
+    document.querySelector('.counts-header').innerHTML = equation + ")"
+  }else {
+    document.querySelector('.counts-header').innerHTML = equation = equation + "("
+  }
+}
+
+const calculate = ()  => {
+  const result = document.querySelector('.counts-header').innerHTML
+  if (result) {
+    verifyPercentage()
   } else {
     document.getElementById('result').innerHTML = ''
     document.querySelector('.counts-header').innerHTML = ''
